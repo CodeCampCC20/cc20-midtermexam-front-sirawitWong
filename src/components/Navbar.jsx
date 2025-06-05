359b6758ef9de2c3c18d8e37cb8deb232a736419
SirawitWong <sirawit.wong.tt@gmail.com>
2025-06-05 17:26:22 +0700

    fix color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
         listStyle : "hover:font-medium hover:text-light"
     }
     return (
-        <div className="navbar bg-accent-100 shadow-sm flex items-center gap-6">
+        <div className="navbar bg-info shadow-sm flex items-center gap-6 mb-4">
             <NavLink className={style.listStyle} to={"/"}>Home</NavLink>
             <NavLink className={style.listStyle} to={"favorite"}>Favorite</NavLink>
             <NavLink className={style.listStyle} to={"login"}>{isLogin ? "Logout" : "Login"}</NavLink>
